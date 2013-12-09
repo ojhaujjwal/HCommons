5c45b0ebe72e9e19813c181f66056d727193e609
Ujjwal Ojha <bring2uo@gmail.com>
2013-12-08 19:31:05 -0800

    Create AbstractPaginatorList.php

---
+++ src/HCommons/Model/AbstractPaginatorList.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace HCommons\Model;
+
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
+use Zend\Db\TableGateway\TableGateway;
+
+
+abstract class AbstractPaginatorList
+{
+
+    /*
+    **  @var bool $deriveResultSetPrototype
+    **  whether or not get ResultSetPrototype from TableGateway
+    **  @see protected function getPaginationAdapter
+    */
+    protected $deriveResultSetPrototype = true;
+
+    protected $tableGateway;
+
+    protected $fields;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->setTableGateway($tableGateway);
+    }
+
+    public function setTableGateway(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function getTableGateway()
+    {
+        return $this->tableGateway;
+    }
+
+    public function setFields(array $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+
+    public function getList(\Closure $anonummous = NULL)
+    {
+        $select = new Select($this->tableGateway->getTable());
+        if ($anonummous) {
+            $anonummous($select);
+        } elseif(method_exists($this, "UseSelect")) {
+            $this->UseSelect($select);
+        } elseif (!empty($this->getFields())) {
+            $select->columns($this->getFields());
+        }
+        // create a new pagination adapter object
+        $paginatorAdapter = $this->getPaginationAdapter($select);
+
+        $paginator = new Paginator($paginatorAdapter);
+
+        return $paginator;
+
+    }
+
+    protected function getPaginationAdapter($select)
+    {
+        // create a new pagination adapter object
+        if ($this->deriveResultSetPrototype) {
+            $paginatorAdapter = new DbSelect(
+                $select,
+                $this->tableGateway->getAdapter(),
+                $this->tableGateway->getResultSetPrototype()
+            );            
+        } else {
+            $paginatorAdapter = new DbSelect(
+                $select,
+                $this->tableGateway->getAdapter()
+            );             
+        }
+
+        return $paginatorAdapter;        
+    }
+    
+}
+
