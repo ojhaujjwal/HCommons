a1baaf212b17302fb3c3702f6b7486ad781a1ef2
Ujjwal Ojha <bring2uo@gmail.com>
2013-12-13 22:11:53 -0800

    Create CsvModel.php

---
+++ src/HCommons/View/Model/CsvModel.php
@@ -0,0 +1,75 @@
+<?php
+    
+namespace HCommons\View\Model;
+    
+use Zend\View\Model\ViewModel;
+use Traversable;
+use Zend\Stdlib\ArrayUtils;
+
+class CsvModel extends ViewModel
+{
+
+    const DEFAULT_FILENAME = "export.csv";
+
+    /**
+     * Csv probably won't need to be captured into a 
+     * a parent container by default.
+     * 
+     * @var string
+     */
+    protected $captureTo = null;
+
+    /**
+     * Csv is terminal
+     * 
+     * @var bool
+     */
+    protected $terminate = true;  
+    
+    /**
+     * Template to use when rendering this model
+     *
+     * @var string
+     */
+    protected $template = 'download/csv';
+    
+ 
+    /**
+     * Constructor
+     *
+     * @param  array|Traversable $data
+     * @return void
+     */    
+    public function __construct($data = null)
+    {
+        $this->setData($data);
+        $this->setFileName(self::DEFAULT_FILENAME);               
+    }
+    
+    public function setData($data)
+    {
+        
+        if ($data instanceof Traversable) {
+            $data = ArrayUtils::iteratorToArray($data);
+        }
+        if (!is_array($data)) {
+            throw new \InvalidArgumentException(sprintf(
+                '%s: expects an array, or Traversable argument; received "%s"',
+                __METHOD__,
+                (is_object($data) ? get_class($data) : gettype($data))
+            ));
+        }
+        $this->setVariable('results', $data);         
+    }
+
+    public function setFileName($filename)
+    {
+        $this->setOption('filename', $filename);               
+    }
+
+    public function getFileName()
+    {
+        return $this->getOption('filename');
+    }
+              
+}
